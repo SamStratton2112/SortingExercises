b248513dbe5674f72714aa5e1310e2eae3d9256a
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-03-16 21:55:40 -0700

    complete

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    // this loop will run through the array once
+    for(let i = 0; i < arr.length; i++){
+        // determine if anything has been swapped 
+        let swapped = false;
+        // this loop will run through each element in the array and 
+        // decrease the array length with each run 
+        for(let j = 0; j < arr.length -i ; j++){
+            if(arr[j] > arr[j+1]){
+                // switch index values 
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]= temp;
+                swapped = true;
+            }
+        }
+        // break function if nothing was swapped and array is sorted
+        if(!swapped) return arr;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,39 @@
-function merge() {}
+function merge(arr1, arr2){
+    const result = []
+    let i = 0;
+    let j = 0;
+    while( i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            result.push(arr1[i]);
+            // increment for each side
+            i++;
+        }else{
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+    // handle one array shorter than the other 
+    while(i < arr1.length){
+        result.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length){
+        result.push(arr2[j]);
+        j++;
+    }
+    return result; 
+}
 
-function mergeSort() {}
-
+function mergeSort(arr){
+    // basecase:
+    if(arr.length <=1 ) return arr;
+    const mid = Math.floor(arr.length/2);
+    // everything else:
+    // we call mergeSort recursively to ensure that we are
+    // breaking down the array into individual elements so
+    // we can then continue to merge 
+    const left = mergeSort(arr.slice(0,mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left,right);
+}
 module.exports = { merge, mergeSort};
